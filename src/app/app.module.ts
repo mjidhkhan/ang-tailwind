6a5f08b670797e7bef704dc60914418953fa6f16
G3TLeighton <gwl@g3t.uk.com>
2020-01-07 16:35:37 +0000

    created component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { TwowayComponent } from './components/twoway/twoway.component';
+import { ModelBindingComponent } from './components/model-binding/model-binding.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     TwowayComponent,
+    ModelBindingComponent,
    
   ],
   imports: [
---
+++ src/app/components/model-binding/model-binding.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-model-binding',
+  templateUrl: './model-binding.component.html',
+  styleUrls: ['./model-binding.component.scss']
+})
+export class ModelBindingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
