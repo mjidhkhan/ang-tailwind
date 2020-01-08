e3301c35ba20388161b6fdc673b85031dc2da37a
MH Khan <majidhkhan@outlook.com>
2020-01-08 00:51:08 +0000

    updated

--- src/app/components/objectives/objectives.component.ts
+++ src/app/components/objectives/objectives.component.ts
@@ -12,4 +12,23 @@ export class ObjectivesComponent implements OnInit {
   ngOnInit() {
   }
 
+  objectives = [
+    {
+      id: 1,
+      name: 'Component Basics',
+      catagory: 'Components ',
+      icon: 'http://satyr.io/200x200/10?text=Components',
+      covers: [
+        { T1: 'The basics of components and their role' },
+        { T2: 'The @Component decorator and its most' },
+        { T3: 'important properties' },
+        { T4: 'Rendering a component' },
+        { T5: 'Passing data into and out of a component using inputs and outputs' },
+        { T6: 'Customizing components with templates and styling' },
+        { T7: 'Injecting content into a component using projection' },
+      ]
+
+    },
+    
+  ];
 }
