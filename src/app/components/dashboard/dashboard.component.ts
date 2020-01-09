5e0cf044113b48b7f93abee8c022e452cd68d670
G3TLeighton <gwl@g3t.uk.com>
2020-01-09 16:29:01 +0000

    fixed GUI

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -6,15 +6,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-myDisabledValue =true
+  toggleTopic: boolean
+  toggleLoo: boolean;
+  toggleMetri: boolean;
+  toggleCountry: boolean;
+  toggleTwoWa: boolean;
+  toggleHom: boolean;
+  toggleDashboar: boolean;
+  toggleBindin: boolean;
   constructor() { }
 
   ngOnInit() {
-    this.myDisabledValue =true
+   this.toggleTopic = false
+    this.toggleLoo= false
+    this.toggleMetri= false
+    this.toggleCountry= false
+    this.toggleTwoWa= false
+    this.toggleDashboar= false
+    this.toggleBindin= false
   }
 
+ 
   toggleTopics(){
-        this.myDisabledValue = !this.myDisabledValue
+        this.toggleTopic = !this.toggleTopic
+  }
+  toggleCountries(){
+        this.toggleCountry = !this.toggleCountry
+  }
+  toggleMetric(){
+        this.toggleMetri = !this.toggleMetri
+  }
+  toggleTwoWay(){
+        this.toggleTwoWa = !this.toggleTwoWa
+  }
+  toggleLoop(){
+        this.toggleLoo = !this.toggleLoo
+  }
+  toggleHome(){
+        this.toggleHom = !this.toggleHom
+  }
+  toggleDashboard(){
+        this.toggleDashboar = !this.toggleDashboar
+  }
+  toggleBinding(){
+        this.toggleBindin = !this.toggleBindin
   }
 
 }
